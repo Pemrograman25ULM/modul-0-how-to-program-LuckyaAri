78083bf87125e115768bae08baa579904d1eeaff
LuckyaAri <jynx.filewalker@gmail.com>
2025-09-30 10:57:15 +0800

    Add files via upload

---
+++ C/PRAK002-LuckyaAriGS-2510817310015.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char a[] = "Luckya Ari G S";
+    printf("Selamat Pagi, %s \n", a);
+    printf("Selamat Siang, %s \n", a);
+    printf("Selamat Malam, %s \n", a);
+    return 0;
+}
---
+++ C/PRAK004-LuckyaAriGS-2510817310015.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("####################################\n");
+    printf("#                                  #\n");
+    printf("#         Luckya Ari G S           #\n");
+    printf("#         2510817310015            #\n");
+    printf("#                                  #\n");
+    printf("####################################\n");
+    return 0;
+}
